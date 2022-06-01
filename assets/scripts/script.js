08696022333f459f14342440060c09bc20e17719
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-05-31 21:10:44 -0400

    finished current weather card

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,8 @@ let state;
 let lat;
 let lon;
 let units = $('#units').attr('data-unit');
+let unit = 'C';
+let measurement = 'KMH';
 let apiKey = '9e28551fe3b5a81f7e8fffbcca8d2a3d';
 let searchBtn = $('#search');
 let unitsBtn = $('#units');
@@ -17,7 +19,6 @@ function getLocation() {
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
             lat = data[0].lat;
             lon = data[0].lon;
             city = data[0].name;
@@ -32,10 +33,26 @@ function updateCurrentWeather() {
             return response.json();
         })
         .then(function(data) {
+            currentWeather.empty();
             console.log(data);
-            let head = $('h1');
-            head.text(`${city} (${moment().format('D/M/YYYY')})`)
+            let head = $('<h2>');
+            let temp = $('<h5>');
+            let wind = $('<h5>');
+            let humidity = $('<h5>');
+            let uv = $('<h5>');
+
+            head.text(`${city}, ${province}\n(${moment().format('D/M/YYYY')})`);
+            temp.text(`Temp: ${data.current.temp} ${unit}`);
+            wind.text(`Wind: ${data.current.wind_speed} ${measurement}`);
+            humidity.text(`Humidity: ${data.current.humidity}%`);
+            uv.text(`UV Index: ${data.current.uvi}%`);
+
+
             currentWeather.append(head);
+            currentWeather.append(temp);
+            currentWeather.append(wind);
+            currentWeather.append(humidity);
+            currentWeather.append(uv);
         })
 }
 
@@ -49,8 +66,12 @@ searchBtn.on('click', function() {
 unitsBtn.on('click', function() {
     if (units === 'Metric') {
         unitsBtn.attr('data-unit', 'Imperial');
+        unit = 'F';
+        measurement = 'MPH';
     } else {
         unitsBtn.attr('data-unit', 'Metric');
+        unit = 'C';
+        measurement = 'KMH';
     }
     units = unitsBtn.attr('data-unit');
     unitsBtn.text(`Unit: ${units}`);
