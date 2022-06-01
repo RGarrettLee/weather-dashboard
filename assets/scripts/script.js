027f8820626d7179739588e1138782ca4d82629f
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-05-31 22:48:03 -0400

    added outlook functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,6 +10,7 @@ let apiKey = '9e28551fe3b5a81f7e8fffbcca8d2a3d';
 let searchBtn = $('#search');
 let unitsBtn = $('#units');
 let currentWeather = $('#cityShowing');
+let outlook = $('#outlook');
 
 console.log(units);
 
@@ -24,6 +25,7 @@ function getLocation() {
             city = data[0].name;
             province = data[0].state;
             updateCurrentWeather();
+            updateOutlook();
         })
 }
 
@@ -41,7 +43,8 @@ function updateCurrentWeather() {
             let humidity = $('<h5>');
             let uv = $('<h5>');
 
-            head.text(`${city}, ${province}\n(${moment().format('D/M/YYYY')})`);
+            if (province) head.text(`${city}, ${province}\n(${moment().format('D/M/YYYY')})`);
+            else head.text(`${city}\n(${moment().format('D/M/YYYY')})`);
             temp.text(`Temp: ${data.current.temp} ${unit}`);
             wind.text(`Wind: ${data.current.wind_speed} ${measurement}`);
             humidity.text(`Humidity: ${data.current.humidity}%`);
@@ -56,6 +59,36 @@ function updateCurrentWeather() {
         })
 }
 
+function updateOutlook() {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            outlook.empty();
+            for (let i = 0; i < data.list.length; i+=8) {
+                let card = $('<section>');
+                card.addClass('card p-3 m-3');
+                let head = $('<h2>');
+                let temp = $('<h5>');
+                let wind = $('<h5>');
+                let humidity = $('<h5>');
+
+                head.text(`${data.list[i].dt_txt}`);
+                temp.text(`Temp: ${data.list[i].main.temp}`);
+                wind.text(`Wind: ${data.list[i].wind.speed} ${measurement}`);
+                humidity.text(`Humidity: ${data.list[i].main.humidity}%`)
+
+                card.append(head);
+                card.append(temp);
+                card.append(wind);
+                card.append(humidity);
+
+                outlook.append(card);
+            }
+        })
+}
 
 searchBtn.on('click', function() {
     city = $('#city').val();
